d5f005f0497fd1dd69dae44d006d14ff98dd6556
yusuf963 <mohammad963yusuf@gmail.com>
2020-12-20 23:19:18 +0000

    adding file

---
+++ index.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require('apollo-server')
+const gql = require('graphql-tag')
+
+
+const typeDefs = gql`
+  type Query{
+    sayHi: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    sayHi: ()=> 'Hello world'
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+server.listen({ port: 5000 })
+  .then(res =>{
+    console.log(`Server summing as ${ res.url}`)
+})
